800ac619e821b6f2a12240f999947d06180f6d76
Rithesh003 <ritheshreddy.ganta@gmail.com>
2023-02-13 09:57:01 -0800

    fourth changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,28 @@
-import React, { useState, useEffect } from "react";
-import { Button } from "./Button";
+import React, { useState } from "react";
+// import { Button } from "./Button";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 function Navbar() {
   const [click, setClick] = useState(false);
-  const [button, setButton] = useState(true);
+  // const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
 
-  const showButton = () => {
-    if (window.innerWidth <= 960) {
-      setButton(false);
-    } else {
-      setButton(true);
-    }
-  };
+  // const showButton = () => {
+  //   if (window.innerWidth <= 960) {
+  //     setButton(false);
+  //   } else {
+  //     setButton(true);
+  //   }
+  // };
 
-  useEffect(() => {
-    showButton();
-  }, []);
+  // useEffect(() => {
+  //   showButton();
+  // }, []);
 
-  window.addEventListener("resize", showButton);
+  // window.addEventListener("resize", showButton);
 
   return (
     <>
@@ -70,7 +70,7 @@ function Navbar() {
               </Link>
             </li> */}
           </ul>
-          {button && <Button buttonStyle="btn--outline">Search</Button>}
+          {/* {button && <Button buttonStyle="btn--outline">GetStarted</Button>} */}
         </div>
       </nav>
     </>
--- src/components/pages/Evaluation.js
+++ src/components/pages/Evaluation.js
@@ -1,112 +1,3 @@
-// import React, { useState } from "react";
-// import "./evaluation.css";
-
-// const data = [
-//   [
-//     {
-//       id: 1,
-//       name: "John Doe",
-//       age: 32,
-//     },
-//     {
-//       id: 2,
-//       name: "Jane Doe",
-//       age: 28,
-//     },
-//   ],
-//   [
-//     {
-//       id: 3,
-//       name: "Alice Smith",
-//       age: 26,
-//     },
-//     {
-//       id: 4,
-//       name: "Bob Smith",
-//       age: 30,
-//     },
-//   ],
-//   [
-//     {
-//       id: 5,
-//       name: "Aliaqefegce Smith",
-//       age: 26,
-//     },
-//     {
-//       id: 6,
-//       name: "Bob SRGSRGsrGSmith",
-//       age: 30,
-//     },
-//   ],
-// ];
-
-// function App() {
-//   const [index, setIndex] = useState(0);
-//   const [comment, setComment] = useState("");
-
-//   const handleSubmit = () => {
-//     // Submit the evaluation metrics and comment to the server or handle it as necessary
-//     console.log(`Evaluation metrics: ${index + 1}`);
-//     console.log(`Comment: ${comment}`);
-//   };
-
-//   return (
-//     <div className="App">
-//       <table className="data-table">
-//         <thead>
-//           <tr>
-//             <th>ID</th>
-//             <th>Name</th>
-//             <th>Age</th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           {data[index].map((person) => (
-//             <tr key={person.id}>
-//               <td>{person.id}</td>
-//               <td>{person.name}</td>
-//               <td>{person.age}</td>
-//             </tr>
-//           ))}
-//         </tbody>
-//       </table>
-//       <div className="buttons">
-//         <button disabled={index === 0} onClick={() => setIndex(index - 1)}>
-//           Previous
-//         </button>
-//         <button
-//           disabled={index === data.length - 1}
-//           onClick={() => setIndex(index + 1)}
-//         >
-//           Next
-//         </button>
-//       </div>
-//       <div className="comment-section">
-//         <form>
-//           <label htmlFor="metrics">Evaluation Metrics:</label>
-//           <select id="metrics">
-//             <option value="Metric 1">Metric 1</option>
-//             <option value="Metric 2">Metric 2</option>
-//             <option value="Metric 3">Metric 3</option>
-//           </select>
-//         </form>
-//         <br />
-//         <textarea
-//           className="comment-box"
-//           placeholder="Enter your comment here"
-//           value={comment}
-//           onChange={(e) => setComment(e.target.value)}
-//         />
-//         <br />
-//         <button className="submit-button" onClick={handleSubmit}>
-//           Submit
-//         </button>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
 import React, { useState } from "react";
 import "./evaluation.css";
 
@@ -114,48 +5,84 @@ const data = [
   [
     {
       id: 1,
-      name: "John Doe",
-      age: 32,
+      question:
+        "Is it true that people with Alzheimer’s disease are particularly prone to depression?",
+      chatgpt:
+        "Yes, people with Alzheimer's disease are at a higher risk of depression due to changes in the brain,difficulty adapting, social isolation, and other factors. It is important to recognize and treat symptoms of depression.",
+      google:
+        "Experts estimate that up to 40 percent of people with Alzheimer's disease suffer from significant depression. Identifying depression in someone with Alzheimer's can be difficult, since dementia can cause some of the same symptoms.",
+      image: require("./Q1.png"),
+      metrics: "",
+      comment: "",
+    },
+  ],
+  [
+    {
+      id: 2,
+      question:
+        "Is it false that it has been scientifically proven that mental exercise can prevent a person from gettingAlzheimer’s disease? ",
+      chatgpt:
+        "No, its not False. Evidence suggests that mental exercise may delay onset or slow progression of Alzheimer's, but more research is needed to understand the relationship.",
+      google:
+        "Neither education nor brain exercises are a sure way to prevent Alzheimer's. But they may help delay symptoms and keep the mind working better for longer.",
+      image: require("./Q2.png"),
+      metrics: "",
+      comment: "",
     },
   ],
   [
     {
       id: 3,
-      name: "Alice Smith",
-      age: 26,
-    },
-    {
-      id: 4,
-      name: "Bob Smith",
-      age: 30,
-    },
-  ],
-  [
-    {
-      id: 5,
-      name: "Aliaqefegce Smith",
-      age: 26,
-    },
-    {
-      id: 6,
-      name: "Bob SRGSRGsrGSmith",
-      age: 30,
+      question:
+        "Is it true that after symptoms of Alzheimer’s disease appear, the average life expectancy is 6 to 12 years?",
+      chatgpt:
+        "Yes, that is generally true. After Alzheimer's symptoms appear, average life expectancy is 6-12 years, but this can vary depending on various factors.",
+      google:
+        "On average, a person with Alzheimer's lives four to eight years after diagnosis, but can live as long as 20 years, depending on other factors.",
+      image: require("./Q3.png"),
+      metrics: "",
+      comment: "",
     },
   ],
 ];
-
+const imageSources = {
+  1: require("./Q1.png"),
+  2: require("./Q2.png"),
+  3: require("./Q3.png"),
+};
 function App() {
   const [index, setIndex] = useState(0);
-  const [metrics, setMetrics] = useState("");
-  const [comment, setComment] = useState("");
+  const [selectedQuestion, setSelectedQuestion] = useState(data[0][0]);
   const [submitted, setSubmitted] = useState(false);
 
+  function handleClick(id) {
+    if (id === 1) {
+      window.open(
+        `https://www.google.com/search?q=1.+Is+it+true+that+people+with+Alzheimer%E2%80%99s+disease+are+particularly+prone+to+depression%3F&rlz=1C1GCEA_enUS1026US1026&sxsrf=AJOqlzXJIQaKxRmjOW9I6n7LcXe-wnO5Yg%3A1675902328121&ei=eD3kY8DqBtCckPIPx4iOsAw`
+      );
+    } else if (id === 2) {
+      window.open(
+        `https://www.google.com/search?q=2.+Is+it+false+that+it+has+been+scientifically+proven+that+mental+exercise+can+prevent+a+person+from+gettingAlzheimer%E2%80%99s+disease%3F&rlz=1C1GCEA_enUS1026US1026&sxsrf=AJOqlzWVCeML33SerabrTUHtfKLFfUi3og%3A1675902747953&ei=Gz_kY9vUOcbOkPIPiLeIuA8`
+      );
+    } else if (id === 3) {
+      window.open(
+        `https://www.google.com/search?q=Is+it+true+that+after+symptoms+of+Alzheimer%E2%80%99s+disease+appear%2C+the+average+life+expectancy+is+6+to+12+years%3F&rlz=1C1GCEA_enUS1026US1026&sxsrf=AJOqlzUDvoOMX8BHItNAdQ1F9UJQSdZVAQ%3A1675902993713&ei=EUDkY-KBK8XOkPIP7teXyAY`
+      );
+    }
+  }
+
   const handleMetricsChange = (event) => {
-    setMetrics(event.target.value);
+    setSelectedQuestion({
+      ...selectedQuestion,
+      metrics: event.target.value,
+    });
   };
 
   const handleCommentChange = (event) => {
-    setComment(event.target.value);
+    setSelectedQuestion({
+      ...selectedQuestion,
+      comment: event.target.value,
+    });
   };
 
   const handleSubmit = () => {
@@ -164,7 +91,11 @@ function App() {
 
   return (
     <div className="App">
-      <h1> Comparision Between ChatGPT and Google </h1>
+      <video src="/videos/video1.mp4" autoPlay loop muted />
+      <h1 style={{ color: "aliceblue" }}>
+        {" "}
+        Comparision Between ChatGPT and Google{" "}
+      </h1>
       <table>
         <thead>
           <tr>
@@ -178,18 +109,36 @@ function App() {
             <tr key={person.question}>
               <td>{person.question}</td>
               <td>{person.chatgpt}</td>
-              <td>{person.google}</td>
+              <td>
+                {imageSources[person.id] && (
+                  <img
+                    src={imageSources[person.id]}
+                    alt="Google Image"
+                    onClick={() => handleClick(person.id)}
+                  />
+                )}
+              </td>
             </tr>
           ))}
         </tbody>
       </table>
       <div className="buttons">
-        <button disabled={index === 0} onClick={() => setIndex(index - 1)}>
+        <button
+          disabled={index === 0}
+          onClick={() => {
+            setIndex(index - 1);
+            setSelectedQuestion(data[index - 1][0]);
+          }}
+        >
           Previous
         </button>
         <button
           disabled={index === data.length - 1}
-          onClick={() => setIndex(index + 1)}
+          onClick={() => {
+            setIndex(index + 1);
+            setSelectedQuestion(data[index + 1][0]);
+            setSubmitted(false);
+          }}
         >
           Next
         </button>
@@ -198,25 +147,33 @@ function App() {
         <div className="evaluation-form">
           <div>
             <label>Evaluation Metrics:</label>
-            <select value={metrics} onChange={handleMetricsChange}>
+            <select
+              value={selectedQuestion.metrics}
+              onChange={handleMetricsChange}
+            >
               <option value="">--Select--</option>
-              <option value="Excellent">Excellent</option>
-              <option value="Good">Good</option>
-              <option value="Average">Average</option>
-              <option value="Poor">Poor</option>
+              <option value="reliability">reliability</option>
+              <option value="currency">currency</option>
+              <option value="readability">readability</option>
+              <option value="disclosure">disclosure</option>
+              <option value="objectivity">objectivity</option>
+              <option value="relevance">relevance</option>
             </select>
           </div>
           <div>
             <label>Comment:</label>
-            <textarea value={comment} onChange={handleCommentChange} />
+            <textarea
+              value={selectedQuestion.comment}
+              onChange={handleCommentChange}
+            />
           </div>
           <button onClick={handleSubmit}>Submit</button>
         </div>
       )}
       {submitted && (
         <div className="result">
-          <p>Evaluation Metrics: {metrics}</p>
-          <p>Comment: {comment}</p>
+          <p>Evaluation Metrics: {selectedQuestion.metrics}</p>
+          <p>Comment: {selectedQuestion.comment}</p>
         </div>
       )}
     </div>
